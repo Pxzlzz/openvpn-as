b70b55d475cbe8048c7be111271a1ce44009c913
Pxzlz <91290196+Pxzlzz@users.noreply.github.com>
2022-08-14 00:12:37 -0400

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+apt install ca-certificates wget net-tools gnupg -y
+
+wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
+echo "deb http://as-repository.openvpn.net/as/debian focal main">/etc/apt/sources.list.d/openvpn-as-repo.list
+apt update
+
+apt install openvpn-as -y
